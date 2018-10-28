03fc079b51b6c6c201336b2ba178bb5ee155c4eb
TaegyunHa <taegyunha92@gmail.com>
2018-10-28 15:42:02 +0000

    Generated my own message type, it has been compiled and the headerfile has been generated

--- src/my_robot_tutorials/scripts/number_counter.py
+++ src/my_robot_tutorials/scripts/number_counter.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Int64
+from std_srvs.srv import SetBool
 
 class NumCnt:
 	cnt = 0
@@ -16,10 +17,18 @@ def callback_receive_data(msg):
 	new_msg.data = NumCnt.cnt
 	pub.publish(new_msg)
 
+def callback_reset_counter(req):
+	if req.data:
+		NumCnt.cnt = 0
+		return True, "Counter has been reset"
+	return False, "Counter has not been reset"
+
 if __name__ == '__main__':
 	rospy.init_node('number_counter')
 	
 	pub = rospy.Publisher("/number_count", Int64, queue_size=10)
 	sub = rospy.Subscriber("/number", Int64, callback_receive_data)
+
+	reset_service = rospy.Service("/reset_number_count", SetBool, callback_reset_counter)
 	
 	rospy.spin()
